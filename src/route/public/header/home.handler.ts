c3a19db7fb5a67d26482c040e56c3f1133b3f709
mark angelo <76676425+ignacio2366@users.noreply.github.com>
2025-04-27 21:25:36 +0800

    direct commit

--- src/route/public/header/home.handler.ts
+++ src/route/public/header/home.handler.ts
@@ -1,6 +1,7 @@
 
 import { Request, Response } from "express";
 import { LandingHeaderTypes } from "../../../models/landing.types";
+import { ItemCartTypes } from "../../../models/itemcart.types";
 
 export const HeaderLanding = (req: Request, res: Response) => {
     const Mock: LandingHeaderTypes = {
@@ -17,4 +18,70 @@ export const HeaderLanding = (req: Request, res: Response) => {
         res.status(404).json({ msg: "Error Response" });
     }
 
+}
+export const LandingItemList = (req: Request, res: Response) => {
+    const MOCK: ItemCartTypes[] = [
+        {
+            cartNo: 1,
+            image: "",
+            brand: "Puma",
+            title: "lorem ipsum",
+            amount: 7500,
+            newitem: true,
+            sold: 10,
+            review: 150,
+        },
+        {
+            cartNo: 2,
+            image: "",
+            brand: "Puma",
+            title: "lorem ipsum",
+            amount: 1000,
+            newitem: true,
+            sold: 10,
+            review: 150,
+        },
+        {
+            cartNo: 3,
+            image: "",
+            brand: "Puma",
+            title: "lorem ipsum",
+            amount: 7500,
+            newitem: true,
+            sold: 10,
+            review: 150,
+        },
+        {
+            cartNo: 4,
+            image: "",
+            brand: "Puma",
+            title: "lorem ipsum",
+            amount: 7500,
+            newitem: true,
+            sold: 10,
+            review: 150,
+        },
+        {
+            cartNo: 5,
+            image: "",
+            brand: "Puma",
+            title: "lorem ipsum",
+            amount: 7500,
+            newitem: true,
+            sold: 10,
+            review: 150,
+        },
+        {
+            cartNo: 6,
+            image: "",
+            brand: "Puma",
+            title: "lorem ipsum",
+            amount: 7500,
+            newitem: true,
+            sold: 10,
+            review: 150,
+        },
+    ];
+
+    res.status(200).json(MOCK);
 }
--- src/route/public/header/home.ts
+++ src/route/public/header/home.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import * as Home from "./home.handler"
 
 const router = Router();
-router.get('/headerlanding', Home.HeaderLanding)
+router.get('/landingheader', Home.HeaderLanding)
+router.get('/landingitemlist', Home.LandingItemList)
+
 
 export default router;
